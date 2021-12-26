33363c228a4f90af8e18aaa93aaff372236056f1
Jonathan Johnson <jon@khonsulabs.com>
2021-12-26 09:45:34 -0800

    Added basic format test
    I noticed in coverage that technically the serialize() implementation
    was not getting tested fully. The default implementation is sound, and
    is a good minimal approach, but it needs to be overridden by any formats
    that check lengths before allocating, which Bincode supports.
    
    However, I still liked the idea of serialize() offering this capability
    by default, so I just wrote a quick little format that only implements
    the missing methods. Since it returns a value from serialized_size, it
    will follow the other path which most likely other crates will not use
    -- but who knows.

--- crates/transmog/src/lib.rs
+++ crates/transmog/src/lib.rs
@@ -32,5 +32,36 @@ pub trait Format<T>: Send + Sync {
     fn deserialize_from<R: Read>(&self, reader: R) -> Result<T, Self::Error>;
 }
 
-#[cfg(feature = "test-util")]
+#[cfg(any(test, feature = "test-util"))]
 pub mod test_util;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Clone)]
+    struct U64BEFormat;
+
+    impl Format<u64> for U64BEFormat {
+        type Error = std::io::Error;
+
+        fn serialized_size(&self, _value: &u64) -> Result<Option<usize>, Self::Error> {
+            Ok(Some(std::mem::size_of::<u64>()))
+        }
+
+        fn serialize_into<W: Write>(&self, value: &u64, mut writer: W) -> Result<(), Self::Error> {
+            writer.write_all(&value.to_be_bytes())
+        }
+
+        fn deserialize_from<R: Read>(&self, mut reader: R) -> Result<u64, Self::Error> {
+            let mut bytes = [0_u8; 8];
+            reader.read_exact(&mut bytes)?;
+            Ok(u64::from_be_bytes(bytes))
+        }
+    }
+
+    #[test]
+    fn basic_format() {
+        test_util::test_format(U64BEFormat);
+    }
+}

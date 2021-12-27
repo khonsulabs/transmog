aadfc5d9befdbcdf4dbba7f77a9d5fd02e59f316
kl-botsu <botsu@khonsulabs.com>
2021-12-27 21:27:10 +0000

    Deploying to gh-pages from @ khonsulabs/transmog@02681c9d741d6e7318289e561f5d6be1086b29ad 🚀

--- main/implementors/core/default/trait.Default.js
+++ main/implementors/core/default/trait.Default.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["transmog_bincode"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"transmog_bincode/struct.Bincode.html\" title=\"struct transmog_bincode::Bincode\">Bincode</a>","synthetic":false,"types":["transmog_bincode::Bincode"]}];
+implementors["transmog_pot"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"transmog_pot/struct.Pot.html\" title=\"struct transmog_pot::Pot\">Pot</a>","synthetic":false,"types":["transmog_pot::Pot"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

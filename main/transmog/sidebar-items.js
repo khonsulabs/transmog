aa1a5cba25f59c2d41446751005ff35551a5114d
kl-botsu <botsu@khonsulabs.com>
2021-12-30 23:29:14 +0000

    Deploying to gh-pages from @ khonsulabs/transmog@92a7c6c33360b08644d8c3135eddeb7f409e9f5f 🚀

--- main/transmog/sidebar-items.js
+++ main/transmog/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["test_util","Utilities for testing formats. Requires feature `test-util`."]],"trait":[["Format","A serialization format."]]});
+initSidebarItems({"mod":[["test_util","Utilities for testing formats. Requires feature `test-util`."]],"trait":[["BorrowedDeserializer","A deserializer that borrows data when possible."],["Format","A serialization format."],["OwnedDeserializer","A deserializer that does not attempt to borrow data when deserializing."]]});

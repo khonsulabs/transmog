4fbc4e089289ec53ccc8caeaba44c8d95c5d876b
Jonathan Johnson <jon@khonsulabs.com>
2021-12-29 16:29:22 -0800

    Added publish command configuration

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,16 +1,30 @@
 use khonsu_tools::{
-    anyhow,
-    code_coverage::{self, CodeCoverage},
+    universal::{anyhow, DefaultConfig},
     Commands,
 };
 use structopt::StructOpt;
 
 fn main() -> anyhow::Result<()> {
     let command = Commands::from_args();
-    match command {
-        Commands::GenerateCodeCoverageReport {
-            install_dependencies,
-        } => CodeCoverage::<code_coverage::DefaultConfig>::execute(install_dependencies),
-        Commands::InstallPreCommitHook | Commands::Audit { .. } => todo!(),
+    command.execute::<Config>()
+}
+
+enum Config {}
+
+impl khonsu_tools::Config for Config {
+    type Publish = Self;
+    type Universal = DefaultConfig;
+}
+
+impl khonsu_tools::publish::Config for Config {
+    fn paths() -> Vec<String> {
+        vec![
+            String::from("crates/transmog"),
+            String::from("crates/transmog-bincode"),
+            String::from("crates/transmog-cbor"),
+            String::from("crates/transmog-pot"),
+            String::from("crates/transmog-async"),
+            String::from("crates/transmog-versions"),
+        ]
     }
 }
